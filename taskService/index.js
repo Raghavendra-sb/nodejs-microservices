fc6ff5cb670097b9a7890b90a0c30c2f80fdfa96
Raghavendra-sb <raghavendra.sb.07@gmail.com>
2026-03-09 20:37:54 +0530

    chore: initialize package.json for taskService with dependencies and scripts

---
+++ taskService/index.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb://mongodb:27017/tasks")
+.then(()=>{
+    console.log("Mongodb connected !");
+})
+.catch((err)=>{
+    console.log("Eroor occured while connectiong mongodb",err);
+})
+
+const TaskSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    userId: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Task =  mongoose.model("Task",TaskSchema); 
+
+
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
+
+app.post('/addTask',async (req,res)=>{
+    try{
+        const {title,description,userId} = req.body;
+
+        const task = await Task.create({
+            title : title,
+            description : description,
+            userId : userId,
+        })
+
+
+       
+
+        res.status(200).json({
+            success: true,
+            task
+        })
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({
+            success:false
+        })
+    }
+})
+
+
+app.get('/getTasks',async(req,res) =>{
+    const tasks = await Task.find();
+
+    res.status(200).json({
+        success: true,
+        tasks
+    })
+})
+
+app.listen(8080, ()=>{
+    console.log('Server up and running');
+})
